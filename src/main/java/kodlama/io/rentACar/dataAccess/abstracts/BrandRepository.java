d4e5ed9b801892b9f8764ff60e94c3cb3a299870
denizkilicaslan <denizz.kilicaslan@gmail.com>
2023-09-04 23:34:20 +0300

    d

--- src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
+++ src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -1,7 +1,5 @@
 package kodlama.io.rentACar.dataAccess.abstracts;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlama.io.rentACar.entities.concretes.Brand;
