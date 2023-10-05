90caa6e758314650b8010e8f13416a48d647c52d
denizkilicaslan <denizz.kilicaslan@gmail.com>
2023-10-05 15:34:19 +0300

    @CrossOrigin

--- src/main/java/kodlama/io/rentACar/business/requests/CreateModelRequest.java
+++ src/main/java/kodlama/io/rentACar/business/requests/CreateModelRequest.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class CreateModelRequest {
 
     @NotNull
